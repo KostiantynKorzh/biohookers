4f8b424ac0ef9f93d30f1609fb71948b3c6ac0f8
denysIl <cfcmfc@gmail.com>
2019-01-08 20:14:32 +0200

    Comment

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  biohookers
+//
+//  Created by Kostia on 08.01.19.
+//  Copyright © 2019 Kostia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Kostes!\n";
+    return 0;
+}
