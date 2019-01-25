f9315c576d4fd3e34e5d33d3b8095c353dcba227
denysIl <46495825+denysIl@users.noreply.github.com>
2019-01-25 10:29:39 +0200

    PEREMOGA

--- main.cpp
+++ main.cpp
@@ -4,7 +4,70 @@
 #include <string>
 
 using namespace std;
+/*
+void st(int o, string *s1, int n1, int k1, int mas1){
+for(int i=0;i<n1;i++){
+    for(int j=0;j<k1;j++){
+        if(s1[o][j]!=s1[i][j]) mas1[o][i]++;
+    }
+}
+}
+*/
 
+
+int main()
+{
+    int n, k;
+    cout<< "Enter number of proteins"<<endl;
+    cin>>n;
+    int mas [100] [100];
+    cout<< "Enter length of proteins"<<endl;
+    cin>>k;
+    string s[n];
+    for (int i=0; i<n; i++)
+    {
+        cin>>s[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<k; j++)
+        {
+            mas[i][j]=0;
+        }
+    }
+
+
+
+    for(int i=0; i<n; i++)
+    {
+        for(int t=0; t<n; t++)
+        {
+            for(int j=0; j<k; j++)
+            {
+                if(s[i][j]!=s[t][j]) mas[i][t]++;
+            }
+        }
+    }
+
+
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<mas[i][j];
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
+
+
+
+/*
 class Splitting
 {
 public:
@@ -21,6 +84,28 @@ Splitting::Splitting()
 {
     s="";
     v.clear();
+}#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cout<< "Enter number of proteins"<<endl;
+    cin>>n;
+    cout<< "Enter length of proteins"<<endl;
+    cin>>k;
+    string s[n];
+    for (int i=0;i<n;i++) cin>>s[i];
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<k; j++) cout<<s[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
 }
 
 Splitting::Splitting(string s1)
@@ -68,9 +153,13 @@ int main(int argc, const char * argv[])
     }
     vector<char>v1;
     v1=proteins[0].get();
+    for(int i = 0;i<v1.size();i++){
+        cout<<proteins[0].v[i];
+    }
+*/
 //ОПЫТКА ЧЕРЕЗ ГЕТТЕРЫ
 
-    //СРАВНИВАНИЕ БЕЛКОВ (НЕ ОЧЕНЬ РАБОТАЕт, проблема в вызове вектора из класса -- proteins[i].v[j]     и тому подобные вызовы; код комплируется но не работает, крашится на этой строке(скорее всего)
+//СРАВНИВАНИЕ БЕЛКОВ (НЕ ОЧЕНЬ РАБОТАЕт, проблема в вызове вектора из класса -- proteins[i].v[j]     и тому подобные вызовы; код комплируется но не работает, крашится на этой строке(скорее всего)
 /*    for (int i=0; i<n; i++){
 
         for(int j=0; j<k; j++){
@@ -78,5 +167,3 @@ int main(int argc, const char * argv[])
             if(proteins[i].v[j]==proteins[i+1].v[j]) m[i+1] [i]++;
         }
     } */
-    return 0;
-}
